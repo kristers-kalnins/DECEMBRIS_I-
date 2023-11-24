5901f06a80a330f5b50546e4c7d2c59afec2481b
kristers-kalnins <126758739+kristers-kalnins@users.noreply.github.com>
2023-11-24 14:38:33 +0200

    Revert "Create 2.uzdevums.py"
    This reverts commit 9910e968842c9fbc50bcd14187d74947d5d546cb.

---
+++ 1.uzdevums.py
@@ -0,0 +1,11 @@
+sakums = int(input("skaitlis"))
+
+
+
+pirmais=1
+
+
+
+for i in range(1, sakums+1):
+    pirmais += 1
+    print(i)
---
+++ 3.uzdevums.py
@@ -0,0 +1,6 @@
+num = int(input("skaitlis:"))
+
+if num % 2 !=0:
+    print(f"{num} nepara skaitlis")
+else:
+    print(f"{num} para skaitlis")
